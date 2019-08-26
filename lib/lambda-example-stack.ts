c8327fd407cfcc4da363c1bc7b9b08e059d70d6c
natalie-elizabeth <natalie.elizabeth@andela.com>
2019-08-26 13:03:19 +0300

    aws lambda-example

--- lib/lambda-example-stack.ts
+++ lib/lambda-example-stack.ts
@@ -1,9 +1,31 @@
 import cdk = require('@aws-cdk/core');
+import lambda = require('@aws-cdk/aws-lambda');
+import { Rule, Schedule } from '@aws-cdk/aws-events';
+import { LambdaFunction } from '@aws-cdk/aws-events-targets';
+import path = require('path');
 
 export class LambdaExampleStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    // Create Lamdba function
+    const sfLambdaFunction = new lambda.Function(
+      this,
+      "StorefrontLambdaFunc",
+      {
+        code: lambda.Code.asset(path.join(__dirname, '../lambda')),
+        handler: 'example.handler',
+        runtime: lambda.Runtime.NODEJS_8_10
+      });
+
+    const rule = new Rule(
+      this,
+      "Rule",
+      {
+        schedule: Schedule.expression('cron(0 18 ? * MON-FRI *)')
+      });
+
+    rule.addTarget(new LambdaFunction(sfLambdaFunction))
   }
 }
